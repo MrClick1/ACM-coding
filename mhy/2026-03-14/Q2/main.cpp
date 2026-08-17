245ff2dbe9fe8248b65798fdc072900458b86731
MrClick1 <1638627022@qq.com>
2026-08-17 16:46:50 +0800

    完成米哈游 2026-03-14 Q2：删除最少元素形成摆动序列并补充复盘

--- mhy/2026-03-14/Q2/main.cpp
+++ mhy/2026-03-14/Q2/main.cpp
@@ -5,11 +5,41 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // TODO: read input
+    // read input
+    int n;
 
-    // TODO: solve
+    cin >> n;
 
-    // TODO: output
+    vector<int> height(n);
+
+    for (int i = 0; i < n; i++) {
+        cin >> height[i];
+    }
+
+    // solve
+    // 在递增的区间，删除中间的元素，保留两端的元素
+    int keep = 1;
+    int pre = 0;
+    
+    int cur = 0;        // height[i] 和 height[i-1] 的差值符号
+    for (int i = 1; i < n; i++) {
+        if (height[i] - height[i-1] > 0) {
+            cur = 1;
+        }else if (height[i] - height[i-1] < 0) {
+            cur = -1;
+        }else {
+            cur = 0;
+        }
+
+        if (cur == 0) continue;
+        if (pre == 0 || pre * cur < 0) {
+            keep++;
+            pre = cur;
+        }
+    }
+
+    // output
+    cout << n - keep << '\n';
 
     return 0;
 }
