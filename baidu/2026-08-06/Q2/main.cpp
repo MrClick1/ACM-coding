6a3777cf5cf550652fdee7c043e7d159c7adadba
MrClick1 <1638627022@qq.com>
2026-08-20 17:37:14 +0800

    baidu: 完成 2026-08-06 Q2 最小对冲值并修复 64 位溢出

--- baidu/2026-08-06/Q2/main.cpp
+++ baidu/2026-08-06/Q2/main.cpp
@@ -5,11 +5,28 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // TODO: read input
+    // read input
+    int q;
+    cin >> q;
 
-    // TODO: solve
+    // solve
+    for (int i = 0; i < q; i++) {
+        long long m;
+        cin >> m;
+        
+        // 计算 y XOR (m-y) 的最小值
+        // 等价于计算 y & (m-y) 的最大值
+        // 直觉上两个越接近 & 的结果越大
+        if (m % 2 == 0) {
+            cout << 0 << "\n";
+        }else {
+            // m 为奇数，划分为一个 (m+1) / 2 和一个 (m-1) / 2
+            long long y = (m+1) / 2;
+            long long z = (m-1) / 2;
+            cout << (y ^ z) << "\n"; 
+        }
+    }
 
-    // TODO: output
 
     return 0;
 }
